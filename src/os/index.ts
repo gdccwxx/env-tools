6f9cb05efaa384fed93191c1c341d2e26cdde781
gdccwxx <gdccwxx@gmail.com>
2021-10-23 22:01:28 +0800

    feat: ✨add `getOsType` to calculate os type

---
+++ src/os/index.ts
@@ -0,0 +1,40 @@
+export * from './is-android';
+export * from './is-ios';
+export * from './is-linux';
+export * from './is-mac';
+export * from './is-win';
+
+import { userAgent } from 'src/common/utils';
+import isAndroid from './is-android';
+import isIOS from './is-ios';
+import isLinux from './is-linux';
+import isMac from './is-mac';
+import isWin from './is-win';
+
+export enum osType {
+  android = 'android',
+  ios = 'ios',
+  linux = 'linux',
+  mac = 'mac',
+  win = 'win',
+  unknown = 'unknown',
+};
+
+/**
+ * get os type
+ * @param ua userAgent
+ * @returns osType
+ */
+export const getOsType = (ua: string = userAgent): osType => {
+  if (isWin(ua)) return osType.win;
+
+  if (isMac(ua)) return osType.mac;
+
+  if (isAndroid(ua)) return osType.android;
+
+  if (isIOS(ua)) return osType.ios;
+
+  if (isLinux(ua)) return osType.linux;
+
+  return osType.unknown;
+};
