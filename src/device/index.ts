77f339156022a6750b9635075bf3490033e83cbd
gdccwxx <gdccwxx@qq.com>
2021-10-24 20:34:15 +0800

    feat: ✨export `device` and `os` module

---
+++ src/device/index.ts
@@ -0,0 +1,4 @@
+export * from './is-ipad';
+export * from './is-iphone';
+export * from './is-ipod';
+export * from './is-iwatch';
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export * from './browser';
-
+export * from './device';
+export * from './os';
