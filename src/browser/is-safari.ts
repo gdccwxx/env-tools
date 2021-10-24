c93303577281b5d974cc1794151874245993bd70
gdccwxx <gdccwxx@qq.com>
2021-10-24 22:52:29 +0800

    refactor: 🔨

--- src/browser/is-safari.ts
+++ src/browser/is-safari.ts
@@ -1,4 +1,5 @@
 import { userAgent, parameterChecker, JSType, errorBuilder } from '../common/utils';
+import { isChrome } from './is-chrome';
 
 export const isSafari = (ua: string = userAgent): boolean => {
   if (!parameterChecker(ua, JSType.string)) {
@@ -6,12 +7,11 @@ export const isSafari = (ua: string = userAgent): boolean => {
   }
 
   const safariMark = /safari/i.test(ua);
-  const chromeMark = /chrome/i.test(ua);
   
   // because qq, 360 ... browsers have safari mark
   // but they also have chrome mark
   // so a better way to find the real browser is include safari but exclude chrome
-  return safariMark && !chromeMark;
+  return safariMark && !isChrome(ua);
 };
 
 export default isSafari;
